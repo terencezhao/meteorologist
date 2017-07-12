7a1c5f1c5838f3f14745ac176dfa1e42828bc3bf
Terence Zhao <terencezhao@chicagobooth.edu>
2017-07-12 14:56:03 +0000

    changed format to string

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -19,15 +19,15 @@ class ForecastController < ApplicationController
     url = "https://api.darksky.net/forecast/6842c74db30df2e42d7a490dc31acdd3/"+@lat+","+@lng #37.8267,-122.4233
     parsed_data = JSON.parse(open(url).read)
 
-    @current_temperature = parsed_data.dig("currently","temperature")
+    @current_temperature = parsed_data.dig("currently","temperature").to_s
 
-    @current_summary = parsed_data.dig("currently","summary")
+    @current_summary = parsed_data.dig("currently","summary").to_s
 
-    @summary_of_next_sixty_minutes = parsed_data.dig("minutely","summary")
+    @summary_of_next_sixty_minutes = parsed_data.dig("minutely","summary").to_s
 
-    @summary_of_next_several_hours = parsed_data.dig("hourly","summary")
+    @summary_of_next_several_hours = parsed_data.dig("hourly","summary").to_s
 
-    @summary_of_next_several_days = parsed_data.dig("daily","summary")
+    @summary_of_next_several_days = parsed_data.dig("daily","summary").to_s
 
     render("forecast/coords_to_weather.html.erb")
   end
